25519ab21dcab44e8d85ddcf6bd9c26408ff92ee
vssense <zelenkin.sa@phystech.edu>
2023-05-19 16:09:44 +0300

    some fixed

--- main.py
+++ main.py
@@ -196,32 +196,33 @@ def add_header(r):
 
 @app.route("/")
 def root_dir():
-    return subdir("")
+    return redirect("/explorer")
 
-@app.route("/get/")
+@app.route("/explorer/")
 def root_dir1():
     return subdir("")
 
 def file_menu(file_path : str):
-    # keys = dict()
-    # keys.filename = os.path.split(file_path)[-1]
-    # some info about file
-    # 
-    return "in progress..."
+    ext = file_path.split('.')[-1]
+    
+    if ext == 'h' or ext == 'cpp':
+        return "PIZDEC SUDA NAXYI"
+    
+    return "Can't display file content..."
 
-def get_type(path:str, name:str):
-    full_path = os.path.join(path, name)
-    return "dir" if os.path.isdir(full_path) else "file" if os.path.isfile(full_path) else "link" if os.path.islink(full_path) else "?"
-
-@app.route("/get/<path:subpath>")
+@app.route("/explorer/<path:subpath>")
 def subdir(subpath : str):
-    print("!!!!!!!!!!!!", "/"+subpath)
-    realpath = "/" + subpath
+    realpath = '/' + subpath
+    print("!!!", realpath)
+
     if (os.path.isfile("/"+subpath)):
         return file_menu("/"+subpath)
+    
     if (os.path.isdir("/"+subpath)):
         subdirs = []
-        for i in os.listdir("/"+subpath.strip()):
+        dirlist = os.listdir("/"+subpath.strip())
+        dirlist.sort() 
+        for i in dirlist:
             i_path = os.path.join(realpath, i)
             i_params = dict()
             i_params["name"] = i
@@ -231,24 +232,24 @@ def subdir(subpath : str):
                 i_params["type"] = "link"
             elif (os.path.isdir(i_path)):
                 i_params["type_icon"] = "/static/folder.png"
-                i_params["type"] = "directory"
+                i_params["type"] = "folder"
             elif (os.path.isfile(i_path)):
                 i_params["type_icon"] = "/static/paper.png"
                 i_params["type"] = "file"
             else:
                 i_params["type_icon"] = "/static/question.png"
                 i_params["type"] = "undefined type"
-            i_params["href_path"] = "/get" + i_path
-            print(os.path.getmtime(i_path))
+
+            i_params["href_path"] = "/explorer" + i_path
             subdirs.append(i_params)
-        print(subpath)
+        print('bleat', subpath)
         print(os.path.split(subpath))
-        print("yay")
-        print("kek", "/get/" + (os.path.split(subpath)[0] if subpath.strip() else ""))
+
+        # print("kek", '/' + (os.path.split(subpath)[0] if subpath.strip() else ""))
         return render_template("index.html",
-                                curdir="/"+subpath.strip(),
+                                curdir="/explorer/"+subpath.strip(),
                                 subdirs=subdirs, 
-                                up_dir="/get/" + (os.path.split(subpath)[0] if subpath.strip() else ""),
+                                up_dir='/explorer/' + (os.path.split(subpath)[0] if subpath.strip() else ""),
                                 menu=dbase.getMenu()
                               )
 
